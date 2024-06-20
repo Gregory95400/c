efd61da65150621abfcc4f4b7b461d2b04011658
Gregory95400 <126874591+Gregory95400@users.noreply.github.com>
2024-06-20 11:15:00 +0200

    chn

--- job.py
+++ job.py
@@ -1,2 +1,14 @@
-a = 2
-print("coucou", a)
+def moyenne(values):
+    return sum(values) / len(values)
+
+def test_moyenne():
+
+    input1 = [1, 1, 1]
+    result = moyenne(input1)
+
+    assert result == 1
+
+    input2 = [1, 2, 3]
+    result = moyenne(input2)
+
+    assert result == 2
